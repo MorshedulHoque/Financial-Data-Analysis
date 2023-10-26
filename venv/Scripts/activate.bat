35f10adf8197742b7a6a48d6a8449a3c2745ab04
unknown <you@example.comasm21612@gmail.com>
2023-10-26 13:11:30 +0600

    Update

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\DA\Documents\Market\Test\venv
+set VIRTUAL_ENV=D:\Forex\Test\Financial-Data-Analysis\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
